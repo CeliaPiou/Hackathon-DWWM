696f2924408748d347654a248e88b33627e6b557
Célia Leydier <leydier16@gmail.com>
2025-04-25 10:21:40 +0200

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Allez les boys!
+    </>
+  )
+}
+
+export default App
