d0de4a9dfcacd9ac54af2bef02949d2ccb9275bc
wandy <47462838+wandyseven@users.noreply.github.com>
2025-04-25 17:48:16 +0200

    coco fonctionnel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,92 @@
-import { useState } from 'react'
-import './App.css'
-import WebcamAccess from './WebcamAccess'
+import React, { useRef, useState,useEffect} from 'react';
+import './App.css';
+import Webcam from 'react-webcam';
+import {drawRect} from  './utilities';
+
+//import de notre model 
+import * as tf from '@tensorflow/tfjs';
+import * as cocossd from '@tensorflow-models/coco-ssd';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  const runCoco = async () => {
+    const net = await cocossd.load();
+    setInterval(() => {
+      detect(net);
+    },10);
+  };
+  
+  const detect = async (net) => {
+    if (
+      typeof webcamRef.current !== "undefined" && 
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.width =videoWidth;
+      webcamRef.current.height =videoHeight;
+      canvasRef.current.width =videoWidth;
+      canvasRef.current.height =videoHeight;
+      const obj = await net.detect(video);
+
+      const ctx = canvasRef.current.getContext("2d");
+      drawRect(obj, ctx);
+    }
+  };
+
+  useEffect(() => {runCoco()})
+
+  useEffect(() => {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(() => {
+        console.log('Permission demandée (ou accordée)');
+      })
+      .catch((err) => {
+        console.error('Erreur webcam :', err);
+      });
+  }, []);
 
   return (
-    <>
-      Allez les boys! LOL
-      <WebcamAccess />
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <Webcam
+          ref={webcamRef}
+          muted={true} 
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zindex: 8,
+            width: 640,
+            height: 480,
+          }}
+        />
+      </header>
+    </div>
+  );
 }
 
 export default App
